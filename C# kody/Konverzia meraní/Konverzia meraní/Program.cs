87d16b559ca69ff8f39ded4050d718a15973b9bd
Radoslav Šedivý <radoslavsedivy900@gmail.com>
2025-05-19 00:02:47 +0200

    doplnenie
    doplnenie o dva kody
    na konverziu merani z excelu - vyráta priemernu hodnotu zo suboru z udajmy a ulozy do dalsieho suboru
    
    zadanie_Trieda upravuje už existujuci kod diagonaly matice a preraba jeho funkcie na tiredu v ktorej sa vytvori matica aby bola bezpecnejsia a nedali sa menit jej hodnoty z vonka triedy

---
+++ C# kody/Konverzia meraní/Konverzia meraní/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace KonverziaMerani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fsCitanie = null;
+            StreamReader srCitanie = null;
+            FileStream fsZapis = null;
+            StreamWriter swZapis = null;
+
+            try
+            {
+                fsCitanie = new FileStream("vstup.txt", FileMode.Open);
+                srCitanie = new StreamReader(fsCitanie);
+
+                fsZapis = new FileStream("vystup.txt", FileMode.Create);
+                swZapis = new StreamWriter(fsZapis);
+
+                string riadok = null;
+
+                while ((riadok = srCitanie.ReadLine()) != null)
+                {
+                    string[] casti = riadok.Split(' ');
+                    string datumCas = casti[0] + " " + casti[1];
+                    int pocet = casti.Length - 2;
+                    double[] hodnoty = new double[pocet];
+
+                    for (int i = 0; i < pocet; i++)
+                    {
+                        try
+                        {
+                            hodnoty[i] = double.Parse(casti[i + 2].Replace(',', '.'));
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Chybný formát čísla: " + casti[i + 2] + " na dátume: " + datumCas);
+                            break;
+                        }
+                    }
+
+                    double suma = 0;
+                    for (int i = 0; i < pocet; i++)
+                    {
+                        suma += hodnoty[i];
+                    }
+
+                    double priemer = suma / pocet;
+                    double priemerZaokruhleny = Math.Round(priemer,2);
+
+                    swZapis.WriteLine(datumCas + "\t" + pocet + "\t" + priemerZaokruhleny.ToString().Replace('.', ','));
+                }
+
+                Console.WriteLine("Spracovanie dokončené.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Vstupný súbor neexistuje.");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Chyba pri práci so súborom: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Neočakávaná chyba: " + e.Message);
+            }
+            finally
+            {
+                if (swZapis != null) swZapis.Close();
+                if (fsZapis != null) fsZapis.Close();
+                if (srCitanie != null) srCitanie.Close();
+                if (fsCitanie != null) fsCitanie.Close();
+            }
+        }
+    }
+}
---
+++ C# kody/Zadanie_Trieda/Zadanie_Trieda/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Matice_diagonaly
+{
+    class DiagonalyMatice
+    {
+        private int[,] matica;
+
+        public DiagonalyMatice(int velkost)
+        {
+            this.matica = new int[velkost, velkost];
+            NaplnMaticuNahodnymiCislami();
+        }
+
+        private void NaplnMaticuNahodnymiCislami()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < matica.GetLength(0); i++)
+            {
+                for (int j = 0; j < matica.GetLength(1); j++)
+                {
+                    matica[i, j] = rand.Next(1, 100);
+                }
+            }
+        }
+
+        public int[,] GetMatica()
+        {
+            return this.matica;
+        }
+
+        public int SucetHlavnejDiagonaly()
+        {
+            int sucet = 0;
+            for (int i = 0; i < matica.GetLength(0); i++)
+            {
+                sucet += matica[i, i];
+            }
+            return sucet;
+        }
+
+        public int SucetVedlajsejDiagonaly()
+        {
+            int sucet = 0;
+            int velkost = matica.GetLength(0);
+            for (int i = 0; i < velkost; i++)
+            {
+                sucet += matica[i, velkost - 1 - i];
+            }
+            return sucet;
+        }
+    }
+
+    class Program
+    {
+        static void VypisMaticu(int[,] matica)
+        {
+            Console.WriteLine("matica: ");
+            for (int i = 0; i < matica.GetLength(0); i++)
+            {
+                for (int j = 0; j < matica.GetLength(1); j++)
+                {
+                    Console.Write(" {0,2} ", matica[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static int VelkostMatice()
+        {
+        opravchyb:
+            Console.WriteLine("Zadajte velkost matice (max 10): ");
+            try
+            {
+                int size = Convert.ToInt32(Console.ReadLine());
+                if (size < 1 || size > 10)
+                {
+                    Console.WriteLine("Velkost matice musi byt medzi 1 a 10.");
+                    goto opravchyb;
+                }
+                return size;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Neplatny vstup. Zadajte cele cislo.");
+                goto opravchyb;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int size = VelkostMatice();
+            DiagonalyMatice diagonaly = new DiagonalyMatice(size);
+            int[,] matica = diagonaly.GetMatica();
+            VypisMaticu(matica);
+
+            int sucetHlavnej = diagonaly.SucetHlavnejDiagonaly();
+            int sucetVedlajsej = diagonaly.SucetVedlajsejDiagonaly();
+
+            Console.WriteLine("Sucet hlavnej diagonaly: " + sucetHlavnej);
+            Console.WriteLine("Sucet vedlajsej diagonaly: " + sucetVedlajsej);
+
+            if (sucetHlavnej > sucetVedlajsej)
+                Console.WriteLine("Hlavna diagonala ma vacsi sucet.");
+            else if (sucetHlavnej < sucetVedlajsej)
+                Console.WriteLine("Vedlajsia diagonala ma vacsi sucet.");
+            else
+                Console.WriteLine("Obe diagonaly maju rovnaky sucet.");
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
