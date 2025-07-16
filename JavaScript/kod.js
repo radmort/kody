8967632a7956732eb06b598a5e30f9200f051985
Radoslav Šedivý <radoslavsedivy900@gmail.com>
2025-07-16 04:30:26 +0200

    uprava kodu

--- JavaScript/kod.js
+++ JavaScript/kod.js
@@ -1,32 +1,28 @@
- 
- const result = fetch("transactions.json")
-      .then(response => response.json())
-      .then(data => {
-        const result = transform(data);
-        return console.log(result);
-      })
-      
-    
-     
+
+const fs = require('node:fs');
+
+const result = JSON.parse(fs.readFileSync("transactions.json", "UTF-8"));
+console.log(transform(result));
+
 
     function transform(oldData) {
       const newDataSet = separe(oldData, 'year', 'amount', 'createdAt', 'customerName', 'state');
 
       const grouped = newDataSet
-        .filter(item => item.state === "canceled")
+       .filter(item => item.state === "canceled")
         .reduce((newObj, item) => ({
-          ...newObj,
-          [item.year]: [...(newObj[item.year] || []), item]
-        }), {});
+         ...newObj,
+      [item.year]: [...(newObj[item.year] || []), item]
+      }), {});
 
       const groupYear = Object.entries(grouped)
-        .sort((a, b) => (b.year) -  (a.year)) 
-        .map(([year, items]) => [
-          year,
-          items
+        .sort((a, b) => b[0] - a[0])
+         .map(([year, items]) => [
+           year,
+           items
             .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) 
             .map(({ amount, createdAt, customerName }) => ({ amount, createdAt, customerName }))
-        ]);
+         ]);
 
       return Object.fromEntries(groupYear);
     }
